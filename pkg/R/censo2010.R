7de963d0c10cbd719778b12f4de1b9c672586171
cjgb <cjgb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-04-27 23:43:21 +0000

    Added code to read the census
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/microdataes@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/censo2010.R
@@ -0,0 +1,25 @@
+###################################################################
+# cjgb
+# 20140428
+# Reads the microdata for the 2010 Spanish Census
+###################################################################
+
+censo2010 <- function(census.file){
+  
+  file.column  <- create.spss.column(system.file( "metadata", "censo_2010_mdat1.txt", package = "MicroDatosEs" ), encoding = "utf8")
+  file.var     <- create.spss.var(system.file( "metadata", "censo_2010_mdat1.txt", package = "MicroDatosEs" ), encoding = "utf8")
+  file.vals    <- create.spss.vals(system.file( "metadata", "censo_2010_mdat2.txt", package = "MicroDatosEs" ), encoding = "utf8")
+  file.missing <- system.file( "metadata", "censo_2010_mdat3.txt", package = "MicroDatosEs" )
+  
+  censo_2010 <- spss.fixed.file( 
+                              file = census.file,
+                              columns.file = file.column,
+                              varlab.file = file.var,
+                              missval.file = file.missing,
+                              codes.file  = file.vals )
+  
+  cat("As of today, there is an error in the variables CNACE and CNO1.\n")
+
+  as.data.set(censo_2010)
+}
+
